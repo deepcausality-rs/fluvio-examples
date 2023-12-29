7935571d5542b91f29882a12fc5413dff6b25423
Marvin Hansen <marvin.hansen@gmail.com>
2023-12-29 12:22:26 +0800

    Added config manager scaffold
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ flv_components/config_manager/src/lib.rs
@@ -0,0 +1,9 @@
+
+
+// // https://stackoverflow.com/questions/20778771/what-is-the-difference-between-0-0-0-0-127-0-0-1-and-localhost
+// const DEFAULT_HOST: &str = "0.0.0.0";
+//
+// #[derive(Debug, Clone, Eq, PartialEq)]
+// pub struct CfgManager<'l> {
+//
+// }
