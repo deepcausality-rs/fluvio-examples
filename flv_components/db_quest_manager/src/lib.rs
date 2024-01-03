a9a2a9d93d5b2ed4c1962b3b34f4efed1679c360
Marvin Hansen <marvin.hansen@gmail.com>
2024-01-03 19:19:59 +0800

    Updated QuestDBManager constructor.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- flv_components/db_quest_manager/src/lib.rs
+++ flv_components/db_quest_manager/src/lib.rs
@@ -1,12 +1,27 @@
 use common::prelude::DBConfig;
+use questdb::{
+    ingress::{Buffer, SenderBuilder},
+    Result as QuestDBResult,
+};
+use questdb::ingress::Sender;
 
-#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct QuestDBManager {
     db_config: DBConfig,
+    sender: Sender,
 }
 
 impl QuestDBManager {
     pub fn new(db_config: DBConfig) -> Self {
-        Self { db_config }
+
+        let host = db_config.host();
+        let port = db_config.port();
+
+        let sender = SenderBuilder::new(host, port)
+            .connect()
+            .expect("Failed to connect to QuestDB");
+
+        Self { db_config, sender }
     }
 }
+
+
