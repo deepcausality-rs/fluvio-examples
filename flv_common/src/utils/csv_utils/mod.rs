46dbe1f013ebfec9f60fbb6b9d382a6ec48cd3e3
Marvin Hansen <marvin.hansen@gmail.com>
2024-01-04 17:33:06 +0800

    Fixed read_csv_lines in csv utils
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- flv_common/src/utils/csv_utils/mod.rs
+++ flv_common/src/utils/csv_utils/mod.rs
@@ -1,11 +1,12 @@
 use crate::prelude::TradeBar;
+use chrono::{TimeZone, Utc};
 use csv::ReaderBuilder;
 use encoding_rs::UTF_8;
-use std::fmt::Error;
+use rust_decimal::prelude::FromPrimitive;
+use rust_decimal::Decimal;
+use std::error::Error;
 use std::fs;
 use std::path::Path;
-
-const SEMICOLON_DELIMITER: u8 = b';';
 
 /// Reads a CSV file from the given path into a Vec of TradeBar objects.
 ///
@@ -26,9 +27,12 @@ const SEMICOLON_DELIMITER: u8 = b';';
 ///
 /// - Checks if the file exists before trying to read it
 /// - Calls read_csv_lines() to parse the file contents into TradeBar objects
-pub fn read_csv_file(path: &str) -> Result<Vec<TradeBar>, Box<Error>> {
+pub fn read_csv_file(path: &str) -> Result<Vec<TradeBar>, Box<dyn Error>> {
     if !Path::new(&path).exists() {
-        panic!("{}", format!("File {} does not exist", path));
+        panic!(
+            "{}",
+            format!("[csv_utils/read_csv_file]: File {} does not exist", path)
+        );
     }
 
     read_csv_lines(path)
@@ -52,29 +56,51 @@ pub fn read_csv_file(path: &str) -> Result<Vec<TradeBar>, Box<Error>> {
 /// - Pre-allocates the Vec to avoid re-allocations
 /// - Decodes the file contents as UTF-8
 /// - Prints any CSV parsing errors but continues processing
-fn read_csv_lines<P>(path: P) -> Result<Vec<TradeBar>, Box<Error>>
+fn read_csv_lines<P>(path: P) -> Result<Vec<TradeBar>, Box<dyn Error>>
 where
     P: AsRef<Path>,
 {
-    let file = fs::read(path).expect("Could not read file");
+    let file = fs::read(path).expect("[csv_utils/read_csv_lines]: Could not read file");
     let (res, _, _) = UTF_8.decode(&file);
+
     let mut content: Vec<TradeBar> = Vec::with_capacity(500_000); // fixed pre-allocation
 
     let mut rdr = ReaderBuilder::new()
-        .flexible(true)
-        .has_headers(true)
-        .double_quote(true)
-        .delimiter(SEMICOLON_DELIMITER)
+        .has_headers(false)
         .from_reader(res.as_bytes());
 
     for result in rdr.records() {
         match result {
             Ok(record) => {
-                let row: TradeBar = record.deserialize(None).expect("Invalid CSV format");
-                content.push(row);
+                let timestamp = record[0]
+                    .parse::<i64>()
+                    .expect("[csv_utils/read_csv_lines]: Could not parse timestamp");
+
+                let p = record[1]
+                    .parse::<f64>()
+                    .expect("[csv_utils/read_csv_lines]: Could not parse price");
+
+                let v = record[2]
+                    .parse::<f64>()
+                    .expect("[csv_utils/read_csv_lines]: Could not parse volume");
+
+                let date_time = Utc.timestamp_opt(timestamp, 0).unwrap();
+
+                let price = Decimal::from_f64(p)
+                    .expect("[csv_utils/read_csv_lines]: Could not parse price from f64");
+
+                let volume = Decimal::from_f64(v)
+                    .expect("[csv_utils/read_csv_lines]: Could not parse volume from f64");
+
+                let trade_bar = TradeBar::new(date_time, price, volume);
+
+                content.push(trade_bar);
             }
             Err(err) => {
-                println!("error reading CSV line: {}", err);
+                println!(
+                    "[csv_utils/read_csv_lines]: error reading CSV line: {}",
+                    err
+                );
             }
         }
     }
--- flv_components/db_quest_manager/tests/db_quest_manager_tests.rs
+++ flv_components/db_quest_manager/tests/db_quest_manager_tests.rs
@@ -1,7 +1,5 @@
 use common::prelude::DBConfig;
 
-const PATH: &str = "data/Kraken_Trading_History/1INCHUSD.csv";
-
 fn get_local_db_config() -> DBConfig {
     DBConfig::new(9009, "0.0.0.0".into())
 }
