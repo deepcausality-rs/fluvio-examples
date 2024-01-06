be3467ba3bcd336cd7d165f482226501134354b6
Marvin Hansen <marvin.hansen@gmail.com>
2024-01-06 11:34:59 +0800

    Added first sketch of SymbolManager implementation plus notes
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- flv_components/symbol_manager/src/lib.rs
+++ flv_components/symbol_manager/src/lib.rs
@@ -1,2 +1,26 @@
+use std::collections::HashMap;
 
-pub struct SymbolManager {}
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct SymbolManager<'l> {
+    // First hashmap that maps from symbol to index
+    symbol_to_index: HashMap<&'l str, u16>,
+    // Second hashmap that maps from index to symbol
+    index_to_symbol: HashMap<u16, &'l str>,
+}
+
+impl<'l> SymbolManager<'l> {
+    pub fn new() -> SymbolManager<'l> {
+
+        // TODO:
+        // * Connect to the DB
+        // * Load symbols from the DB
+        // * Generate both hashmaps
+        // * Construct the symbol manager
+
+        // Implicitly drop the DB connection to make sure it's closed so that SymbolManager is thread-safe.
+        SymbolManager {
+            symbol_to_index: Default::default(),
+            index_to_symbol: Default::default()
+        }
+    }
+}
