96a2d1f4b3c4b749f8349881f6f5cee163b61a6a
Marvin Hansen <marvin.hansen@gmail.com>
2024-01-17 13:46:29 +0800

    Working on example client for handling error messages
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- flv_client_examples/client_login_errors/src/main.rs
+++ flv_client_examples/client_login_errors/src/main.rs
@@ -3,6 +3,8 @@ use std::process;
 use fluvio::FluvioAdmin;
 use fluvio::metadata::objects::CommonCreateRequest;
 use fluvio::metadata::topic::TopicSpec;
+use client_utils::flv_utils::{create_topic, delete_topic};
+
 mod handle;
 mod handle_login_error;
 mod handle_logout_error;
@@ -19,32 +21,19 @@ async fn run() -> Result<(), Box<dyn Error>> {
 
     println!("Client Setup!");
 
-    // Create a new admin client
-    // Create new client topics:
-    // 1. test-topic
-    // 2. test-topic-control - For control messages
-    // 3. test-topic-data - For receiving data messages
-
-    // OK, I get this one.
-    let admin = FluvioAdmin::connect().await.unwrap();
+    // Create a new admin client to create new client topics:
+    // 1. test-topic-control - For receiving control messages from the QD gateway.
+    // 2. test-topic-data - For receiving data messages from the QD gateway.
+    // 3. test-topic-error - For receiving error messages from the QD gateway.
 
     // Create a new topic
-    let name= String::from("test-topic");
-    let dry_run = true;
-    // WTF is this?
-    let common_request = CommonCreateRequest {
-        name,
-        dry_run,
-        ..Default::default()
-    };
+    let name="test-topic";
 
-    // Ok, specify the topic config
-    let topic_specs = TopicSpec::new_computed(1,1, None);
+    // Create a new topic
+    create_topic(name).await.expect("Failed to create topic");
 
-    // Create the topic
-    admin.create_with_config(common_request, topic_specs)
-        .await
-        .expect("Failed to create topic");
+    // Delete topic before shutting down the client
+    delete_topic(name).await.expect("Failed to delete topic");;
 
     Ok(())
 }
--- flv_client_examples/client_utils/src/flv_utils.rs
+++ flv_client_examples/client_utils/src/flv_utils.rs
@@ -1,6 +1,20 @@
 use fluvio::{Fluvio, FluvioAdmin, PartitionConsumer, RecordKey, TopicProducer};
 use std::error::Error;
+use fluvio::metadata::objects::{CommonCreateRequest, DeleteRequest};
+use fluvio::metadata::topic::config::TopicConfig;
+use fluvio::metadata::topic::TopicSpec;
 
+/// Sends a message to the given Fluvio topic producer.
+///
+/// # Arguments
+///
+/// * `producer` - The Fluvio topic producer to send the message to.
+/// * `buffer` - The message payload as a byte buffer.
+///
+/// # Returns
+///
+/// Returns a Result with `()` on success, or an error on failure.
+///
 pub async fn send_message(producer: &TopicProducer, buffer: Vec<u8>) -> Result<(), Box<dyn Error>> {
     producer
         .send(RecordKey::NULL, buffer)
@@ -12,12 +26,78 @@ pub async fn send_message(producer: &TopicProducer, buffer: Vec<u8>) -> Result<(
     Ok(())
 }
 
-// Note that this may fail if you are not authorized as a Fluvio administrator
-// for the cluster you are connected to.
-pub async fn get_admin() -> FluvioAdmin {
-    FluvioAdmin::connect().await.unwrap()
+/// Creates a new topic with the given name on the Fluvio cluster.
+///
+/// # Arguments
+///
+/// * `topic_name` - The name for the new topic to create.
+///
+/// # Returns
+///
+/// Returns a Result with `()` on success, or an error on failure.
+///
+pub async fn create_topic(topic_name: &str) -> Result<(), Box<dyn Error>> {
+
+    // Instantiate the admin client
+    let admin = FluvioAdmin::connect()
+        .await
+        .expect("Failed to connect to Fluvio admin API");
+
+    // Define a new topic
+    let name = topic_name.to_string();
+    let dry_run = false;
+    let common_request = CommonCreateRequest {
+        name,
+        dry_run,
+        ..Default::default()
+    };
+
+    // Ok, specify the topic config
+    let topic_specs = TopicSpec::new_computed(1, 1, None);
+
+    // Create the topic
+    admin.create_with_config(common_request, topic_specs)
+        .await
+        .expect("Failed to create topic");
+
+    Ok(())
 }
 
+pub async fn delete_topic(topic_name: &str) -> Result<(), Box<dyn Error>> {
+
+    // Instantiate the admin client
+    let admin = FluvioAdmin::connect()
+        .await
+        .expect("Failed to connect to Fluvio admin API");
+
+    let name = topic_name.to_string();
+
+
+    let topic_config = TopicConfig::default();
+
+    let topic_specs = TopicSpec::from(topic_config);
+
+    let delete_topic_request: DeleteRequest<TopicSpec> = DeleteRequest::new(name);
+
+
+    // Delete the topic
+    admin.delete(delete_topic_request).await.expect("Failed to delete topic");
+    //
+    //^^^^^^ cannot infer type for type parameter `S` declared on the method `delete`
+
+    Ok(())
+}
+
+/// Creates a new Fluvio topic producer for the given topic name.
+///
+/// # Arguments
+///
+/// * `topic` - The name of the Fluvio topic to produce to.
+///
+/// # Returns
+///
+/// Returns a `TopicProducer` that can be used to produce records to the topic.
+///
 pub async fn get_producer(topic: &str) -> TopicProducer {
     let fluvio = Fluvio::connect().await.unwrap();
 
@@ -27,6 +107,16 @@ pub async fn get_producer(topic: &str) -> TopicProducer {
         .expect("Failed to create a producer")
 }
 
+/// Creates a new partition consumer for the given Fluvio topic.
+///
+/// # Arguments
+///
+/// * `topic` - The name of the Fluvio topic to consume from.
+///
+/// # Returns
+///
+/// Returns a `PartitionConsumer` for consuming records from the topic.
+///
 pub async fn get_consumer(topic: &str) -> PartitionConsumer {
     fluvio::consumer(topic, 0)
         .await
