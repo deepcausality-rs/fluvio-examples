658c588a8f93a30844d1ac185532d6832ac67600
Marvin Hansen <marvin.hansen@gmail.com>
2024-01-17 14:46:15 +0800

    Working on QDClient crate.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- flv_client/src/lib/mod.rs
+++ flv_client/src/lib/mod.rs
@@ -1,6 +1,17 @@
+use std::error::Error;
+use fluvio::{FluvioAdmin, TopicProducer};
+use common::prelude::MessageClientConfig;
+
 mod utils;
 mod login;
 
-pub struct QDClient{
+pub struct QDClient {
+    admin: FluvioAdmin,
+    client_config: MessageClientConfig,
+    consumer: Result<(), Box<dyn Error + Send>>,
+    producer: TopicProducer,
+}
+
+impl QDClient {
 
 }
