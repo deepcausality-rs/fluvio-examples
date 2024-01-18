7808e15338e142186ae021d35e1d6fc03eb67928
Marvin Hansen <marvin.hansen@gmail.com>
2024-01-18 20:44:37 +0800

    Updated sample code.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- flv_qd_client_examples/basic_data_stream/src/main.rs
+++ flv_qd_client_examples/basic_data_stream/src/main.rs
@@ -1,4 +1,4 @@
-use common::prelude::MessageClientConfig;
+use common::prelude::{ExchangeID, MessageClientConfig};
 use qd_client::QDClient;
 use std::time::Duration;
 use tokio::time::sleep;
@@ -8,20 +8,28 @@ mod handle_error_channel;
 
 const CLIENT_ID: u16 = 42;
 
+const ETH_AED: u16 = 278;
+//  278 = ethaed on Kraken
+
+
 #[tokio::main]
 async fn main() {
     let client_config = MessageClientConfig::new(CLIENT_ID);
 
-    //
+    // Create a QD Gateway client
     let data_handler = handle_data_channel::handle_data_event;
     let error_handler = handle_error_channel::handle_error_event;
-    //
-    // Create a QD Gateway client
     let client = QDClient::new(CLIENT_ID, client_config, data_handler, error_handler)
         .await
         .expect("Failed to create QD Gateway client");
 
-    // client.start_trade_data().await.expect("Failed to start trade data");
+    // Start streaming trade data
+    // Add Kraken to ExchangeID
+    // let exchange_id = ExchangeID::COINBASE;
+    // let symbol_id = ETH_AED;
+    // client.start_trade_data(exchange_id, symbol_id)
+    //     .await
+    //     .expect("Failed to start trade data");
 
     // Wait a moment ..
     sleep(Duration::from_secs(5)).await;
