443656c6be805cc561ee5fca12dfd508499b28c4
Marvin Hansen <marvin.hansen@gmail.com>
2024-01-22 15:31:36 +0800

    Added minimal QD client setup to causal_data_inference.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- flv_qd_client_examples/causal_data_inference/src/main.rs
+++ flv_qd_client_examples/causal_data_inference/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+use tokio::time::sleep;
+use client_utils::{handle_error_utils, handle_utils, print_utils};
+use common::prelude::MessageClientConfig;
+use qd_client::QDClient;
+
+mod handle_data;
+
+const EXAMPLE: &'static str = "Causal Data Inference";
+
+const FN_NAME: &'static str = "causal_data_inference/main";
+
+const CLIENT_ID: u16 = 77;
+
+#[tokio::main]
+async fn main() {
+    print_utils::print_example_header(EXAMPLE);
+
+    println!("{FN_NAME}: Build Client config for client ID: {CLIENT_ID}",);
+    let client_config = MessageClientConfig::new(CLIENT_ID);
+
+    println!("{FN_NAME}: Build QD Client",);
+    let client = QDClient::new(CLIENT_ID, client_config.clone())
+        .await
+        .expect("basic_data_stream/main: Failed to create QD Gateway client");
+
+    // println!("{FN_NAME}: Start the data handler",);
+    // let data_topic = client_config.data_channel();
+    // tokio::spawn(async move {
+    //     if let Err(e) = handle_utils::handle_channel(&data_topic, handle_data_message).await {
+    //         eprintln!("[QDClient/new]: Consumer connection error: {}", e);
+    //     }
+    // });
+
+    println!("{FN_NAME}: Start the error handler",);
+    let err_topic = client_config.error_channel();
+    tokio::spawn(async move {
+        if let Err(e) =
+            handle_utils::handle_channel(&err_topic, handle_error_utils::handle_error_message).await
+        {
+            eprintln!("[QDClient/new]: Consumer connection error: {}", e);
+        }
+    });
+
+    println!("{FN_NAME}: Wait a moment to let the OHLCV data stream complete...");
+    sleep(Duration::from_secs(2)).await;
+
+    println!("{FN_NAME}: Closing client");
+    client.close().await.expect("Failed to close client");
 }
