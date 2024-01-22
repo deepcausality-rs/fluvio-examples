6f74b64429dca68868758632a69783ef9afded52
Marvin Hansen <marvin.hansen@gmail.com>
2024-01-22 15:25:55 +0800

    Updated example script
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/example.sh
+++ scripts/example.sh
@@ -6,15 +6,16 @@ set -o pipefail
 # Bash Select (Make Menu) https://linuxize.com/post/bash-select/
 
 echo ""
-echo "--------------------------------"
-echo "Select number of the example to run: "
-echo "--------------------------------"
-echo "base: Basic Data Stream Example"
-echo "quit: Exit"
-echo "--------------------------------"
+echo "-----------------------------------------"
+echo "Select the number of the example to run: "
+echo "-----------------------------------------"
+echo "1) base: Basic Data Stream Example"
+echo "2) causal: Real-Time Causal Inference Example"
+echo "3) quit: Exit"
+echo "-----------------------------------------"
 echo ""
 
-select opt in  base quit;
+select opt in  base causal quit;
 do
   case $opt in
 
@@ -23,6 +24,12 @@ do
       command cargo run --release --bin basic_data_stream
       break
       ;;
+
+    causal)
+            echo "Selected example: Causal Inference QD Client Example"
+            command cargo run --release --bin causal_data_inference
+            break
+            ;;
 
     quit)
       echo "Exiting!"
