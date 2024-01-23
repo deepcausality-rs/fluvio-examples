429dca302713cd1f0f4a5b005e6453ec8bb71a87
Marvin Hansen <marvin.hansen@gmail.com>
2024-01-23 20:53:20 +0800

    Added type extensions to causal inference example.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

---
+++ flv_qd_client_examples/causal_data_inference/src/lib/extensions/indexable.rs
@@ -0,0 +1,52 @@
+use crate::prelude::Indexable;
+use crate::types::alias::CustomContext;
+
+impl Indexable for CustomContext<'_> {
+    fn get_current_year_index(&self) -> usize {
+        todo!()
+    }
+
+    fn get_current_month_index(&self) -> usize {
+        todo!()
+    }
+
+    fn get_current_day_index(&self) -> usize {
+        todo!()
+    }
+
+    fn set_current_year_index(&mut self, _index: usize) {
+        todo!()
+    }
+
+    fn set_current_month_index(&mut self, _index: usize) {
+        todo!()
+    }
+
+    fn set_current_day_index(&mut self, _index: usize) {
+        todo!()
+    }
+
+    fn get_previous_year_index(&self) -> usize {
+        todo!()
+    }
+
+    fn get_previous_month_index(&self) -> usize {
+        todo!()
+    }
+
+    fn get_previous_day_index(&self) -> usize {
+        todo!()
+    }
+
+    fn set_previous_year_index(&mut self, _index: usize) {
+        todo!()
+    }
+
+    fn set_previous_month_index(&mut self, _index: usize) {
+        todo!()
+    }
+
+    fn set_previous_day_index(&mut self, _index: usize) {
+        todo!()
+    }
+}
