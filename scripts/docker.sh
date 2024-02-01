3ce8802b3ede63c7a41f18f1f76858b21ba03b99
Marvin Hansen <marvin.hansen@gmail.com>
2024-02-01 19:58:02 +0800

    Updated Dockerfile & script.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,6 +7,6 @@ set -o pipefail
 # Copy a temporary Dockerfile to the root directory
 command cp flv_specs/service_specs/src/services/qdgw/Dockerfile Dockerfile_qdgw
 # Build the image with the Docker deamon.
-command docker build -t cmdb:latest -f Dockerfile_qdgw .
+command docker build -t qdgw:latest -f Dockerfile_qdgw .
 # Remove temporary Dockerfile
 command rm Dockerfile_qdgw
