609e325dfe36811aaa2dc30bb1d2e4a47cf548bc
Marvin Hansen <marvin.hansen@gmail.com>
2024-02-06 19:12:17 +0800

    Added module level documentation to flv_common
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- flv_common/src/types/symbol_types/mod.rs
+++ flv_common/src/types/symbol_types/mod.rs
@@ -1,1 +1,9 @@
+/// Symbol types
+///
+/// This module defines common types for working with symbols and symbol metadata.
+/// 
+/// # Types
+///
+/// - `Symbol` - Main symbol struct containing symbol metadata like name, id, etc.
+///
 pub mod symbol;
