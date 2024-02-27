ef33fe703901913e2639fe69f44dc303e01246d0
Marvin Hansen <marvin.hansen@gmail.com>
2024-02-27 12:19:36 +0800

    Updated query generators in csv import crate
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- flv_cli/csv_import/src/bin/csv_import/query_gen.rs
+++ flv_cli/csv_import/src/bin/csv_import/query_gen.rs
@@ -1,40 +1,36 @@
 pub(crate) fn generate_trade_table_ddl(table_name: &str) -> String {
-    format!(
-        r"
+    format!(r"
         CREATE TABLE IF NOT EXISTS default.{table_name}
         (
-           `timestamp` Datetime64(3) CODEC(DoubleDelta),
-           `price` Float64 CODEC(Gorilla),
-           `volume` Float64 CODEC(Gorilla),
+           `timestamp` Datetime64(3) CODEC(DoubleDelta, LZ4),
+           `price`  Float64 CODEC(Delta, LZ4),
+           `volume` Float64 CODEC(Delta, LZ4),
         )
      ENGINE = MergeTree()
      PRIMARY KEY toStartOfHour(timestamp)
-     "
-    )
+     ")
 }
 
 pub(crate) fn generate_insert_query(file: &str, path: &str) -> String {
     let table_name = format!("KRAKEN_{}", file).to_lowercase();
-    format!(
-        r"
+    format!(r"
         INSERT INTO {table_name} (timestamp, price, volume)
         SELECT timestamp, price, volume
         FROM
         file('{path}', 'CSV', 'timestamp Datetime64(3), price Float64, volume Float64')
-        "
-    )
+        ")
 }
 
 pub(crate) fn generate_count_query(path: &str) -> String {
-    format!(
-        "SELECT count(*) FROM file('{path}', 'CSV', 'timestamp Datetime64(3), price Float64, volume Float64')"
-    )
+    format!(r"
+    SELECT count(*) FROM file('{path}', 'CSV', 'timestamp Datetime64(3), price Float64, volume Float64')
+    ")
 }
 
 pub(crate) fn generate_metadata_table_ddl(meta_data_table: &str) -> String {
-    format!(
-        r"
-        CREATE TABLE IF NOT EXISTS {meta_data_table} (
+    format!(r"
+        CREATE TABLE IF NOT EXISTS {meta_data_table}
+        (
             symbol String,
             symbol_id UInt64,
             table_name String,
@@ -43,6 +39,5 @@ pub(crate) fn generate_metadata_table_ddl(meta_data_table: &str) -> String {
         ENGINE = MergeTree
         PRIMARY KEY (symbol, symbol_id)
         SETTINGS index_granularity=128;
-        ",
-    )
+        ")
 }
