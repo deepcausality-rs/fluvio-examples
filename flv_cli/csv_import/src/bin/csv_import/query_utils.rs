be757c9a86c969258df8fc51c3b570d713c73551
Marvin Hansen <marvin.hansen@gmail.com>
2024-02-27 13:55:58 +0800

    Code formatting
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- flv_cli/csv_import/src/bin/csv_import/query_utils.rs
+++ flv_cli/csv_import/src/bin/csv_import/query_utils.rs
@@ -5,6 +5,7 @@ use std::error::Error;
 
 pub(crate) async fn count_rows(client: &Client, path: &str) -> Result<u64, Box<dyn Error>> {
     let count_query = query_gen::generate_count_query(path);
+
     let number_of_rows: CountRow = client
         .query_one(&count_query)
         .await
@@ -18,6 +19,7 @@ pub(crate) async fn create_trade_data_table(
     table_name: &str,
 ) -> Result<(), Box<dyn Error>> {
     let query = query_gen::generate_trade_table_ddl(table_name);
+
     client
         .execute(&query)
         .await
@@ -32,7 +34,11 @@ pub(crate) async fn insert_trade_data(
     path: &str,
 ) -> Result<(), Box<dyn Error>> {
     let query = query_gen::generate_insert_query(&file, &path);
-    client.execute(&query).await.expect("Failed to insert data");
+
+    client
+        .execute(&query)
+        .await
+        .expect("[insert_trade_data]: Failed to insert data");
 
     Ok(())
 }
@@ -42,6 +48,7 @@ pub(crate) async fn create_meta_data_table(
     meta_data_table: &str,
 ) -> Result<(), Box<dyn Error>> {
     let query = query_gen::generate_metadata_table_ddl(meta_data_table);
+
     client
         .execute(&query)
         .await
@@ -56,12 +63,12 @@ pub(crate) async fn insert_meta_data(
     meta_data_table: &str,
 ) -> Result<(), Box<dyn Error>> {
     let query = query_gen::generate_meta_data_insert_query(meta_data_table);
-    let rows = vec![meta_data.clone()];
+    let rows = vec![meta_data];
 
     client
         .insert_native_block(query, rows)
         .await
-        .expect("Failed to write meta data");
+        .expect("[insert_meta_data] Failed to write meta data");
 
     Ok(())
 }
